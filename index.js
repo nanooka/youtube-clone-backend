8cee3f355d96bf69ae903f241f5475881ca6ca6b
nanooka <102428722+nanooka@users.noreply.github.com>
2024-05-20 01:23:05 +0400

    added links

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const apiKey = "AIzaSyCB_jwO0CDx7oIHM3wUXTlU0zwiJOh12x8";
+
 // API endpoint to fetch YouTube data
 app.get("/api/youtube/search", async (req, res) => {
   try {
     const { query } = req.query;
-    const apiKey = "AIzaSyCB_jwO0CDx7oIHM3wUXTlU0zwiJOh12x8";
+    // const apiKey = "AIzaSyCB_jwO0CDx7oIHM3wUXTlU0zwiJOh12x8";
 
     // Make request to YouTube Data API to search videos
     const response = await axios.get(
@@ -23,7 +25,7 @@ app.get("/api/youtube/search", async (req, res) => {
           key: apiKey,
           part: "snippet",
           type: "video",
-          maxResults: 10, // Adjust as needed
+          maxResults: 20, // Adjust as needed
         },
       }
     );
@@ -31,6 +33,48 @@ app.get("/api/youtube/search", async (req, res) => {
     res.json(response.data);
   } catch (error) {
     console.error("Error fetching YouTube data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/videos/:videoId", async (req, res) => {
+  try {
+    const { videoId } = req.params;
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/videos",
+      {
+        params: {
+          id: videoId,
+          key: apiKey,
+          part: "snippet,statistics,player,contentDetails",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching video by videoId:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/channels/:channelId", async (req, res) => {
+  try {
+    const { channelId } = req.params;
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/channels",
+      {
+        params: {
+          id: channelId,
+          key: apiKey,
+          part: "snippet,id,statistics",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching video by videoId:", error);
     res.status(500).json({ error: "Internal server error" });
   }
 });
