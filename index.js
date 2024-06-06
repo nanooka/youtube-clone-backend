e92ede959a941099c9717f2b3e34a269ec9aa10e
nanooka <102428722+nanooka@users.noreply.github.com>
2024-06-07 01:19:33 +0400

    created new endpoint of search in channel page

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ const apiKey = "AIzaSyCB_jwO0CDx7oIHM3wUXTlU0zwiJOh12x8";
 app.get("/api/youtube/search", async (req, res) => {
   try {
     const { query } = req.query;
-    // const apiKey = "AIzaSyCB_jwO0CDx7oIHM3wUXTlU0zwiJOh12x8";
-
-    // Make request to YouTube Data API to search videos
     const response = await axios.get(
       "https://www.googleapis.com/youtube/v3/search",
       {
@@ -25,8 +22,39 @@ app.get("/api/youtube/search", async (req, res) => {
           key: apiKey,
           part: "snippet",
           type: "video",
-          maxResults: 20, // Adjust as needed
+          maxResults: 20,
         },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching YouTube data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/channel-search", async (req, res) => {
+  try {
+    const { query, channelId } = req.query;
+
+    const params = {
+      q: query,
+      key: apiKey,
+      part: "snippet",
+      type: "video",
+      maxResults: 20,
+    };
+
+    // Add channelId to params if it's provided
+    if (channelId) {
+      params.channelId = channelId;
+    }
+
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params,
       }
     );
 
@@ -100,6 +128,180 @@ app.get("/api/youtube/comments/:videoId", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+app.get("/api/youtube/channel-videos/:channelId", async (req, res) => {
+  try {
+    const { channelId } = req.params;
+    const { order = "date" } = req.query;
+
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          channelId: channelId,
+          key: apiKey,
+          part: "snippet",
+          order: order,
+          maxResults: 20,
+        },
+      }
+    );
+
+    const videoIds = response.data.items
+      .map((item) => item.id.videoId)
+      .join(",");
+
+    const videoDetailsResponse = await axios.get(
+      "https://www.googleapis.com/youtube/v3/videos",
+      {
+        params: {
+          id: videoIds,
+          key: apiKey,
+          part: "snippet,contentDetails,statistics",
+        },
+      }
+    );
+
+    res.json(videoDetailsResponse.data);
+  } catch (error) {
+    console.error("Error fetching videos for channel:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/channel-playlists/:channelId", async (req, res) => {
+  try {
+    const { channelId } = req.params;
+
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/playlists",
+      {
+        params: {
+          channelId: channelId,
+          key: apiKey,
+          part: "snippet,contentDetails",
+          maxResults: 25,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching playlists for channel:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/channel-playlistItems/:playlistId", async (req, res) => {
+  try {
+    const { playlistId } = req.params;
+
+    const response = await axios.get(
+      "https://www.googleapis.com/youtube/v3/playlistItems",
+      {
+        params: {
+          playlistId: playlistId,
+          key: apiKey,
+          part: "snippet,contentDetails",
+          maxResults: 25,
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching playlists for channel:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/api/youtube/channel-live/:channelId", async (req, res) => {
+  try {
+    const { channelId } = req.params;
+    const { order = "date" } = req.query;
+
+    const searchResponse = await axios.get(
+      "https://www.googleapis.com/youtube/v3/search",
+      {
+        params: {
+          channelId: channelId,
+          key: apiKey,
+          part: "snippet",
+          eventType: "completed",
+          type: "video",
+          order: order,
+          maxResults: 20,
+        },
+      }
+    );
+
+    const videoIds = searchResponse.data.items
+      .map((item) => item.id.videoId)
+      .join(",");
+
+    const videoDetailsResponse = await axios.get(
+      "https://www.googleapis.com/youtube/v3/videos",
+      {
+        params: {
+          id: videoIds,
+          key: apiKey,
+          part: "snippet,contentDetails,statistics,liveStreamingDetails",
+        },
+      }
+    );
+
+    console.log("Video Details Response:", videoDetailsResponse.data);
+
+    res.json(videoDetailsResponse.data);
+  } catch (error) {
+    console.error("Error fetching live videos for channel:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// app.get("/api/youtube/videos-details", async (req, res) => {
+//   try {
+//     const { videoIds } = req.query; // Expect a comma-separated list of video IDs
+//     const response = await axios.get(
+//       "https://www.googleapis.com/youtube/v3/videos",
+//       {
+//         params: {
+//           id: videoIds,
+//           key: apiKey,
+//           part: "snippet,contentDetails,statistics",
+//         },
+//       }
+//     );
+//     const videoDetails = response.data.items;
+
+//     const channelIds = [
+//       ...new Set(videoDetails.map((video) => video.snippet.channelId)),
+//     ];
+//     const channelResponse = await axios.get(
+//       "https://www.googleapis.com/youtube/v3/channels",
+//       {
+//         params: {
+//           id: channelIds.join(","),
+//           key: apiKey,
+//           part: "snippet",
+//         },
+//       }
+//     );
+
+//     const channelDetails = channelResponse.data.items.reduce((acc, channel) => {
+//       acc[channel.id] = channel;
+//       return acc;
+//     }, {});
+
+//     res.json({
+//       videoDetails,
+//       channelDetails,
+//     });
+//   } catch (error) {
+//     console.error("Error fetching video details:", error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
