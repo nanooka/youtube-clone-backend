e3e7a0177c8189ebd7943b2ea7cbef396b917666
nanooka <102428722+nanooka@users.noreply.github.com>
2024-08-10 13:16:31 +0400

    bcrypt update

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { ObjectId } = require("mongodb");
 const { connectToDb, getDb } = require("../db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
